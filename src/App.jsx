f2d9ee07a082ab056f497da2998bd37e7e41b1d7
biswass101 <biswassnaeemcse@gmail.com>
2025-03-06 13:13:02 +0600

    completed auth page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import { Home, Auth, Orders, Tables, Menu } from "./pages";
 import Header from "./components/shared/Header";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header/>
+    <Router>
+      <AppContent />
+    </Router>
+  );
+}
+
+function AppContent() {
+  const location = useLocation();
+  const hideHeaderRoutes = ["/auth"];
+
+  return (
+    <>
+      {!hideHeaderRoutes.includes(location.pathname) && <Header />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
-        <Route path="/orders" element={<Orders/>} />
-        <Route path="/tables" element={<Tables/>} />
-        <Route path="/menu" element={<Menu/>}/>
-        <Route path="*" element={<div>Route not Found</div>}/>
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/tables" element={<Tables />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="*" element={<div>Route not Found</div>} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
